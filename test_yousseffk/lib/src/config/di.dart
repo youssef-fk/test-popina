d9966a664bbdde6ee2e28b69c21ed5d91101a1b0
youssef-fk <filali.khattabi.youssef@gmail.com>
2021-12-17 17:42:33 +0100

    Added order service logic and refactored

--- test_yousseffk/lib/src/config/di.dart
+++ test_yousseffk/lib/src/config/di.dart
@@ -1,10 +1,8 @@
 import 'package:get/get.dart';
-import 'package:test_yousseffk/src/cache/order_cache_service.dart';
 import 'package:test_yousseffk/src/network/orders_network_service.dart';
 import 'package:test_yousseffk/src/services/orders_service.dart';
 
 Future<void> initDependencies() async {
-  Get.lazyPut(() => OrderCacheService());
   Get.lazyPut(() => OrdersNetworkService());
   Get.lazyPut(() => OrdersService());
 }
--- test_yousseffk/lib/src/services/orders_service.dart
+++ test_yousseffk/lib/src/services/orders_service.dart
@@ -1,3 +1,27 @@
 import 'package:get/get.dart';
+import 'package:test_yousseffk/src/models/order_model.dart';
+import 'package:test_yousseffk/src/network/orders_network_service.dart';
 
-class OrdersService extends GetxService {}
+class OrdersService extends GetxService {
+  final OrdersNetworkService _ordersNetworkService = Get.find();
+
+  final rxOrderList = <OrderModel>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    /// Initial call for populating the data
+    refreshOrders();
+  }
+
+  Future<void> refreshOrders() async {
+    try {
+      final data = await _ordersNetworkService.getOrders();
+
+      rxOrderList.value = data;
+    } catch (e) {
+      Get.showSnackbar(GetSnackBar(title: e.toString()));
+    }
+  }
+}
